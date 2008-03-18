478c086333d59a36a8a8dc708e56036d9ba0faeb
x11 <x11@a439b4bb-83d8-46c6-86ec-f6d4d22dddae>
2008-03-18 13:50:24 +0000

    Bugfix
    git-svn-id: svn://arturaz.net/public/rails/plugins/ufo_helper@564 a439b4bb-83d8-46c6-86ec-f6d4d22dddae

--- init.rb
+++ init.rb
@@ -1,4 +1,4 @@
-source = File.join('public', 'ufo.js')
+source = File.join(File.dirname(__FILE__), 'public', 'ufo.js')
 dest = File.join(RAILS_ROOT, 'public', 'javascripts', 'ufo.js')
 FileUtils.cp(source, dest) unless File.exists?(dest)
 
